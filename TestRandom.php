9258e0e0b8513c47d471598414e1762aa4714a8f
root <hanqing002@ke.com>
2018-10-10 21:28:25 +0800

    隐藏了API key

--- TestRandom.php
+++ TestRandom.php
@@ -3,13 +3,13 @@
 require_once 'Randomizer.php';
 use  luffy\random\Randomizer;
 
-$rnd=new Randomizer('449c131f-0171-401e-80c9-212f6427e654');
-$data=$rnd->integers(10,1,100,10);
+$rnd=new Randomizer('449c131f-0171-401e-80c9-xxxxxxxx');
+//$data=$rnd->integers(10,1,100,100);
 //$data=$rnd->decimalFractions(10,6);
 //$data=$rnd->gaussians(20,0,1,5);
 //$ch='abcdefghijklmnopqrstuvwxyz';
 //$data=$rnd->strings(10,8,$ch);
 //$data=$rnd->uuids(10);
 //$data=$rnd->blobs(2,1024);
-//$data=$rnd->usage();
+$data=$rnd->usage();
 var_dump($data);
